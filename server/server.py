18cb6c1073dafe7a4d2a77f9a08d7064ea52e99a
Sid-Myla <119138398+Sid-Myla@users.noreply.github.com>
2024-11-16 17:33:22 -0600

    firebase gang stuff ykwim

--- server/server.py
+++ server/server.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+from firebase_admin import credentials, firestore
+import firebase_admin
 
 app = Flask(__name__)
+
+cred = credentials.Certificate("./serviceapi.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 @app.route('/')
 def home():
     return "Hello, Flask from server.py!"
 
-@app.route('/status')
-def status():
-    return "Server is up and running!"
+@app.route('/firebase')
+def get_users():
+    users_ref = db.collection('user')  
+    docs = users_ref.stream()
+    users = {doc.id: doc.to_dict() for doc in docs}
+    
+    return jsonify(users)
 
 
 if __name__ == '__main__':
